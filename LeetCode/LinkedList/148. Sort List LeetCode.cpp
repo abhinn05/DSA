f47c046f7b031976d5923fd01ba670e6c7e977cc
Abhinn Goyal <158797889+abhinn05@users.noreply.github.com>
2025-02-05 15:26:31 +0530

    Add files via upload

---
+++ LeetCode/LinkedList/148. Sort List LeetCode.cpp
@@ -0,0 +1,31 @@
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        vector<int> a;
+        ListNode* temp = head;
+        while(temp)
+        {
+            a.push_back(temp->val);
+            temp = temp->next;
+        }
+        sort(a.begin(),a.end());
+        temp = head;
+        for(int val : a)
+        {
+            temp->val = val;
+            temp = temp->next;
+        }
+        return head;
+    }
+};
