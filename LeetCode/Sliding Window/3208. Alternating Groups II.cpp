8055c8e26d053b6b9d2251560a1a5f4b572fff73
Abhinn Goyal <158797889+abhinn05@users.noreply.github.com>
2025-06-13 10:38:47 +0530

    Add files via upload

---
+++ LeetCode/Sliding Window/3208. Alternating Groups II.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool alter(vector<int>& window)
+    {
+        for (int i = 0; i < window.size() - 1; i++) 
+        {
+            if (window[i] == window[i + 1]) return false;
+        }
+        return true;
+    }
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        int ans = 0;
+        for(int i=0;i<k-1;i++)
+        {
+            colors.push_back(colors[i]);
+        }
+        int n = colors.size();
+        
+        int left=0;
+        int right=1;
+        while(right<n)
+        {
+            if(colors[right] == colors[right-1])
+            {
+                left = right;
+                right++;
+                continue;
+            }
+            right++;
+
+            if(right - left < k) continue;
+
+            ans++;
+            left++;
+        }
+        
+        return ans;
+    }
+};
