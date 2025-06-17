1df777907b48ef0fa152107421bf13a3990a6ff6
Abhinn Goyal <158797889+abhinn05@users.noreply.github.com>
2025-06-17 16:41:31 +0530

    Add files via upload

--- LeetCode/Arrays/74. Search a 2D Matrix.cpp
+++ LeetCode/Arrays/74. Search a 2D Matrix.cpp
@@ -3,42 +3,17 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int row = -1;
+        int row = 0;
+        int col = m-1;
 
-        if(n==1)
+        while(row<n && col>=0)
         {
-            int l = 0;
-            int r = m-1;
-            while(l<=r)
-            {
-                if(matrix[0][l] == target || matrix[0][r] == target) return true;
-                l++;
-                r--;
-            }
-            return false;
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(matrix[i][0] > target)
-            {
-                row = i-1;
-                break;
-            }
-            else if(matrix[i][0] == target) return true;
-        }
-        if(matrix[n-1][0] < target)
-        {
-            row = n-1;
-        }
-        if(row<0) return false;
-        int l = 0;
-        int r = m-1;
-        while(l<=r)
-        {
-            if(matrix[row][l] == target || matrix[row][r] == target) return true;
-            l++;
-            r--;
+            if(matrix[row][col] == target) return true;
+
+            else if(matrix[row][col] > target) col--;
+            else if(matrix[row][col] < target) row++;
         }
         return false;
+        
     }
 };
