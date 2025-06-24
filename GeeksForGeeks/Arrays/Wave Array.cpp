3169f9f074a05080e4abfcbcab78e303b1c9a999
Abhinn Goyal <158797889+abhinn05@users.noreply.github.com>
2025-06-24 11:39:25 +0530

    Add files via upload

---
+++ GeeksForGeeks/Arrays/Wave Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    // arr: input array
+    // Function to sort the array into a wave-like array.
+    void convertToWave(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        for(int i=0;i<n-1;i=i+2)
+        {
+            swap(arr[i],arr[i+1]);
+        }
+    }
+};
