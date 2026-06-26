88b2c36c3e678953c1d4cb52bf07514380b31c9b
Abhinn Goyal <158797889+abhinn05@users.noreply.github.com>
2026-06-26 19:51:20 +0530

    Add files via upload

---
+++ LeetCode/Arrays/3739. Count Subarrays With Majority Element II.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> freq(2 * n+1,0);
+
+        freq[n] = 1;
+        int idx = n;
+
+        long long res = 0, pref = 0;
+
+        for(int x : nums)
+        {
+            if(x == target)
+            {
+                pref += freq[idx];
+                idx++;
+            }
+            else
+            {
+                idx--;
+                pref -= freq[idx];
+            }
+            freq[idx]++;
+            res += pref;
+        }    
+        return res;
+    }
+};
